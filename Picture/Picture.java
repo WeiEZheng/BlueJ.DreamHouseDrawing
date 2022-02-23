42286f4b28d8e670b0e847f8d2dcf345bb334f99
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-02-22 23:19:31 -0500

    moved instructions for the house to the right location inside picture rather than picture test

--- Picture/Picture.java
+++ Picture/Picture.java
@@ -12,10 +12,15 @@
  */
 public class Picture
 {
-    private Square wall;
-    private Square window;
-    private Triangle roof;
     private Circle sun;
+    private Circle doorwindow;
+    private Square door;
+    private Square window;
+    private Square window2;
+    private Square wall;
+    private Square wall2;
+    private Triangle roof;
+    private Triangle roof2;
 
     /**
      * Constructor for objects of class Picture
@@ -30,13 +35,20 @@ public class Picture
      */
     public void draw()
     {
+    sun = new Circle();
+        sun.changeColor("yellow");
+        sun.moveHorizontal(20);
+        sun.moveVertical(-10);
+        sun.changeSize(60);
+        sun.makeVisible();
+        
         wall = new Square();
         wall.moveVertical(80);
         wall.changeSize(100);
         wall.makeVisible();
-
+        
         window = new Square();
-        window.changeColor("black");
+        window.changeColor("white");
         window.moveHorizontal(20);
         window.moveVertical(100);
         window.makeVisible();
@@ -45,14 +57,40 @@ public class Picture
         roof.changeSize(50, 140);
         roof.moveHorizontal(60);
         roof.moveVertical(70);
+        roof.changeColor("black");
         roof.makeVisible();
-
-        sun = new Circle();
-        sun.changeColor("yellow");
-        sun.moveHorizontal(180);
-        sun.moveVertical(-10);
-        sun.changeSize(60);
-        sun.makeVisible();
+        
+        wall2 = new Square();
+        wall2.moveVertical(60);
+        wall2.moveHorizontal(100);
+        wall2.changeSize(120);
+        wall2.makeVisible();
+        
+        window2 = new Square();
+        window2.changeColor("white");
+        window2.moveVertical(100);
+        window2.moveHorizontal(70);
+        window2.makeVisible();
+        
+        roof2 = new Triangle();
+        roof2.changeSize(50, 180);
+        roof2.moveHorizontal(170);
+        roof2.moveVertical(60);
+        roof2.changeColor("black");
+        roof2.makeVisible();
+        
+        door = new Square();
+        door.moveVertical(130);
+        door.moveHorizontal(130);
+        door.changeColor("blue");
+        door.changeSize(50);
+        door.makeVisible();
+        
+        doorwindow = new Circle();
+        doorwindow.moveVertical(80);
+        doorwindow.moveHorizontal(180);
+        doorwindow.changeColor("white");
+        doorwindow.makeVisible();
     }
 
     /**
--- Picture/PictureTest.java
+++ Picture/PictureTest.java
@@ -8,20 +8,11 @@ import org.junit.Test;
 /**
  * The test class PictureTest.
  *
- * @author  Wei Zheng
- * @version 2/22/2022
+ * @author  
+ * @version 
  */
 public class PictureTest
 {
-    private Circle sun;
-    private Circle doorwindow;
-    private Square door;
-    private Square window;
-    private Square window2;
-    private Square wall;
-    private Square wall2;
-    private Triangle roof;
-    private Triangle roof2;
     
     /**
      * Default constructor for test class PictureTest
@@ -55,62 +46,7 @@ public class PictureTest
     {
         Picture picture1 = new Picture();
         picture1.draw();
-        sun = new Circle();
-        sun.changeColor("yellow");
-        sun.moveHorizontal(20);
-        sun.moveVertical(-10);
-        sun.changeSize(60);
-        sun.makeVisible();
         
-        wall = new Square();
-        wall.moveVertical(80);
-        wall.changeSize(100);
-        wall.makeVisible();
-        
-        window = new Square();
-        window.changeColor("white");
-        window.moveHorizontal(20);
-        window.moveVertical(100);
-        window.makeVisible();
-
-        roof = new Triangle();
-        roof.changeSize(50, 140);
-        roof.moveHorizontal(60);
-        roof.moveVertical(70);
-        roof.changeColor("black");
-        roof.makeVisible();
-        
-        wall2 = new Square();
-        wall2.moveVertical(60);
-        wall2.moveHorizontal(100);
-        wall2.changeSize(120);
-        wall2.makeVisible();
-        
-        window2 = new Square();
-        window2.changeColor("white");
-        window2.moveVertical(100);
-        window2.moveHorizontal(70);
-        window2.makeVisible();
-        
-        roof2 = new Triangle();
-        roof2.changeSize(50, 180);
-        roof2.moveHorizontal(170);
-        roof2.moveVertical(60);
-        roof2.changeColor("black");
-        roof2.makeVisible();
-        
-        door = new Square();
-        door.moveVertical(130);
-        door.moveHorizontal(130);
-        door.changeColor("blue");
-        door.changeSize(50);
-        door.makeVisible();
-        
-        doorwindow = new Circle();
-        doorwindow.moveVertical(80);
-        doorwindow.moveHorizontal(180);
-        doorwindow.changeColor("white");
-        doorwindow.makeVisible();
     }
 }
 
